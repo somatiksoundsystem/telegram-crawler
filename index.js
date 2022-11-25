1673b1c742eb2a814b92468b0653af75e4f125b9
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2022-11-25 15:26:19 +0400

    cleanup

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-import {Telegraf} from "telegraf"
-import {config as dotenv} from "dotenv"
-import {debug} from "./src/debug.js";
-import {post} from "./src/channel-post.js";
+import {Telegraf} from 'telegraf'
+import {config as dotenv} from 'dotenv'
+import {debug} from './src/debug.js'
+import {post} from './src/channel-post.js'
 
 dotenv()
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.use(debug)
 bot.use(post)
 
-bot.launch().catch((e) => {
-    console.error(e.message)
-    console.error(e)
-
-    process.exit(1)
-})
+bot.launch().catch((e) => bot.stop(e.message))
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+process.once(`SIGINT`, () => bot.stop(`SIGINT`))
+process.once(`SIGTERM`, () => bot.stop(`SIGTERM`))
 
 const me = await bot.telegram.getMe()
 console.log(`Bot is started https://t.me/${me.username}!`)
