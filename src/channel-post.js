4e982886be40fc241fd13879840edc24e8bc2cd8
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2023-01-25 13:21:24 +0530

    process photos

--- src/channel-post.js
+++ src/channel-post.js
@@ -1,16 +1,28 @@
 const saveOrUpdateChannelPost = (post, isNew) => {
     console.log(post)
-    console.log(`${isNew ? `New` : `Updated`} channel post: ${post.text}`)
+    console.log(`${isNew ? `New` : `Updated`} channel post: ${post.text || post.caption}`)
+    return post
+}
+
+const getPhotoLinks = async (post, ctx) => {
+    if (post && post.photo) {
+        const files = post.photo.map((it) => ctx.telegram.getFileLink(it.file_id));
+        for await (const file of files) {
+            console.log(file)
+        }
+    }
 }
 
 export const post = (ctx) => {
     const update = ctx.update
+    let post;
     switch (ctx.updateType) {
         case `channel_post`:
-            saveOrUpdateChannelPost(update.channel_post, true)
+            post = saveOrUpdateChannelPost(update.channel_post, true)
             break
         case `edited_channel_post`:
-            saveOrUpdateChannelPost(update.edited_channel_post, false)
+            post = saveOrUpdateChannelPost(update.edited_channel_post, false)
             break
     }
+    getPhotoLinks(post, ctx).catch((e) => console.error(e));
 }
